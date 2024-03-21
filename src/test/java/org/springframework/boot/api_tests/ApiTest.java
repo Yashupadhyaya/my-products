d223cbdf53674d52515ee963be6c167f0765811f
Yashupadhyaya <50600305+Yashupadhyaya@users.noreply.github.com>
2024-03-21 17:12:37 +0530

    fixes

--- src/test/java/org/springframework/boot/api_tests/ApiTest.java
+++ src/test/java/org/springframework/boot/api_tests/ApiTest.java
@@ -1,23 +1,24 @@
 
-  package org.springframework.boot.api_tests;
+package org.springframework.boot.api_tests;
 
-  import com.intuit.karate.Results;
-  import com.intuit.karate.Runner;
-  // import com.intuit.karate.http.HttpServer;
-  // import com.intuit.karate.http.ServerConfig;
-  import org.junit.jupiter.api.Test;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
 
-  import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-  class ApiTest {
+class ApiTest {
 
-      @Test
-      void testAll() {
-          String apiHostServer = System.getenv().getOrDefault("API_HOST", http://localhost:8080);
-          Results results = Runner.path("classpath:org/springframework/boot/api_tests")
-                  .systemProperty("url.base", apiHostServer)
-                  .parallel(1);
-          assertEquals(0, results.getFailCount(), results.getErrorMessages());
-      }
+    @Test
+    void testAll() {
+        String apiHostServer = System.getenv().getOrDefault("API_HOST",
+                "https://virtserver.swaggerhub.com/061REB413/employee-service/0.1");
+        Results results = Runner.path("classpath:org/springframework/boot/api_tests")
+                .systemProperty("url.base", apiHostServer)
+                .parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
 
-  }
+}
